9b4b37d32562659101ff871accf8fb9996f8d35a
K.Author <kim15883082@naver.com>
2024-04-16 17:35:59 +0900

    특성 테이블 코드 생성

---
+++ Assets/Tables/TraitTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tables
+{
+    [Serializable]
+    public struct TraitOptionData
+    {
+        public int battleOptionType;
+        public int value1;
+        public int value2;
+    }
+    
+    [Serializable]
+    public struct TraitData
+    {
+        public int id;
+        public int traitType;
+        public bool traitCondition;
+        public int traitVive;
+        public int type;
+        public TraitOptionData[] optionDatas;
+        public int rarity;
+        public int sort;
+        public int group;
+
+    }
+    
+    public class TraitTable:ScriptableObject
+    {
+        [SerializeField] private TraitData[] traitDatas;
+    }
+}
